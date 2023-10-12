fef6435b0c1d6a0e4faa8677a1772426c15364f6
EWHF <over_rak@mail.ru>
2023-10-13 00:05:24 +0500

    сделал табы

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,19 @@
+const play = document.querySelector('.play');
+const preview = document.querySelector('.preview');
 
-
-const play = document.querySelector(".play");
-
-const preview = document.querySelector(".preview");
-
-play.addEventListener("click", (event) => {
-  
-  play.classList.add("hide");
-
-  preview.classList.add("hide");
-
-  document.querySelector("iframe").src="https://www.youtube.com/embed/7cIVLb9qPQY?autoplay=1";
-  
+play.addEventListener('click', (event) => {
+  play.classList.add('hide');
+  preview.classList.add('hide');
+  document.querySelector('iframe').src='https://www.youtube.com/embed/7cIVLb9qPQY?autoplay=1';
 })
 
+const tabs = document.querySelectorAll('.js-tab');
+
+tabs.forEach(button => {
+  button.addEventListener('click',()=> {
+    document.querySelector('.js-tab.active').classList.remove('active');
+    document.querySelector('.js-tab-content.active').classList.remove('active');
+    button.classList.add('active');
+    document.querySelector(`[data-tab-content="${button.dataset.tabButton}"]`).classList.add('active');
+  });
+});
